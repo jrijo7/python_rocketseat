627422d299175553dce3c9e15b32dad49fa5ff0e
João Rijo <joaovictor_gomes.7@hotmail.com>
2025-02-24 23:53:23 -0300

    sintaxe do python

---
+++ sintaxe.py
@@ -0,0 +1,8 @@
+# Esse é meu primeiro comentário
+"""Esse é meu primeiro comentário
+   Essa é a segunda linha de comentário"""
+
+nome = 'Gabriel' + \
+' Casemiro'
+
+nome = 'Gabriel' + ' Casemiro'
