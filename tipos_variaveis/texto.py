93746835bb57571cbe5428a103d29ae06115cf76
João Rijo <joaovictor_gomes.7@hotmail.com>
2025-03-04 22:14:01 -0300

    sobre declarações

---
+++ tipos_variaveis/texto.py
@@ -0,0 +1,11 @@
+# Declaração
+nome_completo = "Gabriel Casemiro"
+
+nome_completo_aspas = """Gabriel
+Casemiro"""
+
+numero_completo_quebra = "Gabriel \
+  Casemiro"
+
+nome = "Gabriel"
+sobrenome = "Casemiro"
